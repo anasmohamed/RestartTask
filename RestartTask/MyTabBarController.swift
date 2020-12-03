d32203469eb4c6b4bd3c35b582cea43ea8db9269
Anas Mohamed <anaskamal659@gmail.com>
2020-12-03 12:50:55 +0200

    tab bar

---
+++ RestartTask/MyTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  MyTabBarController.swift
+//  RestartTask
+//
+//  Created by Anas on 12/3/20.
+//  Copyright © 2020 Anas. All rights reserved.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
